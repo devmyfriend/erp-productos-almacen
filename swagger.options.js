6b9f6a1cd44b0c4c779627b17e62e408badf6a13
Sebastian Matinez <desarrollofullstack@bencorp.com.mx>
2024-02-23 11:00:33 -0500

    update: swagger options

--- swagger.options.js
+++ swagger.options.js
@@ -1,7 +1,7 @@
 export const options = {                                                                                                        definition: {
     openapi: '3.0.0',
     info: {
-            title: 'ERP API PRODUCTOS Y ALMACÉN',
+            title: 'ERP-API PRODUCTOS Y ALMACÉN',
             version: '1.0.0',
             description: 'ERP-API',
     },
