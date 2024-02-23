2055cb65c9415b1192099ed11327283f58ba6e8a
Sebastian Matinez <desarrollofullstack@bencorp.com.mx>
2024-02-23 10:55:39 -0500

    feat: products template

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,15 +1,25 @@
-
 import { ProductModel } from '../models/products.model.js';
 
 const findAll = async (req, res) => {
 	try {
 		const data = await ProductModel.findAll({
+			attributes: [
+				'CodigoProducto',
+				'TipoProductoId',
+				'NombreProducto',
+				'LineaId',
+				'CreadoEn',
+			],
 			where: {
 				Borrado: 0,
 			},
 		});
 
-		return res.status(200).json({ response: data } );
+		if (data.length < 1) {
+			return res.status(404).json({ message: 'No hay datos disponibles' });
+		}
+
+		return res.status(200).json({ response: data });
 	} catch (error) {
 		console.log(error);
 
