fdedf9e0d55a566fb9ce17993e3af99331e7276d
Sebastian Matinez <desarrollofullstack@bencorp.com.mx>
2024-02-23 11:01:35 -0500

    update: swagger options

--- swagger.options.js
+++ swagger.options.js
@@ -1,20 +1,19 @@
-export const options = {                                                                                                        definition: {
-    openapi: '3.0.0',
-    info: {
-            title: 'ERP-API PRODUCTOS Y ALMACÉN',
-            version: '1.0.0',
-            description: 'ERP-API',
-    },
-    servers: [
-
-            {
-                    url: 'http://localhost:5000',
-            },
-            {
-                    url: 'http://lachosoft.cloud:7000/',
-            }
-
-    ],
-},
-apis: ['./src/routes/*.js'],
+export const options = {
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'ERP-API PRODUCTOS Y ALMACÉN',
+			version: '1.0.0',
+			description: 'ERP-API',
+		},
+		servers: [
+			{
+				url: 'http://localhost:5000',
+			},
+			{
+				url: 'http://lachosoft.cloud:7000/',
+			},
+		],
+	},
+	apis: ['./src/routes/*.js'],
 };
